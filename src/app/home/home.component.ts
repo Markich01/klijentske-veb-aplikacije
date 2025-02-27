a9e3e4d79bd3caaeafa517b75047109d111e0200
Markich01 <lazarevicmilos77@gmail.com>
2025-02-27 19:39:46 +0100

    novo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { FlightService } from '../../services/flight.service';
+import { JsonPipe, NgIf } from '@angular/common';
+import { AxiosError } from 'axios';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [JsonPipe, NgIf],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  service = FlightService
+  public flights: any[] | null = null
+  public error: string | null = null
+
+  constructor(){
+    FlightService.getFlights()
+      .then(rsp=> this.flights = rsp.data)
+      .catch((e: AxiosError) => this.error = `${e.code}: ${e.message}`)
+  }
 }
